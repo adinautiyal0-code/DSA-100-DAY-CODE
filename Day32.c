3c52e4f9775d5b47725d234a59a8ea90d40645a9
Aditya Nautiyal <adinautiyal0@gmail.com>
2026-03-28 10:55:50 +0530

    Day_32

---
+++ Day32.c
@@ -0,0 +1,59 @@
+// Implement push and pop operations on a stack and verify stack operations.
+
+#include <stdio.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Push operation
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = value;
+}
+
+// Pop operation
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    top--;
+}
+
+// Display stack
+void display() {
+    for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+}
+
+int main() {
+    int n, m, x;
+
+    // Number of elements to push
+    scanf("%d", &n);
+
+    // Push elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        push(x);
+    }
+
+    // Number of pops
+    scanf("%d", &m);
+
+    // Perform pop operations
+    for (int i = 0; i < m; i++) {
+        pop();
+    }
+
+    // Display remaining stack
+    display();
+
+    return 0;
+}
